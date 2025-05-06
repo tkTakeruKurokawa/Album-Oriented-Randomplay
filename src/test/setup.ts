a7f3e1aa51d474a906c24cb33462d697be1b48cb
Kurokawa Takeru <40245979+tkTakeruKurokawa@users.noreply.github.com>
2025-05-06 21:47:06 +0900

    fix: testing-libraryのmatchersインポート方法を修正

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { expect, afterEach, beforeAll, afterAll } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { server } from '../mocks/server';
 
 // Extend Vitest's expect with jest-dom matchers
