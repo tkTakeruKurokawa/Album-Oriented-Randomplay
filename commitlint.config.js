642989c95882d1e19a45aa360063a912de8416ba
Kurokawa Takeru <40245979+tkTakeruKurokawa@users.noreply.github.com>
2026-03-08 22:21:36 +0900

    feat: commitlintの設定を変更

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,6 @@ export default {
   rules: {
     'body-max-line-length': [2, 'always', 200], // 本文の行の長さを最大200文字に設定
     'footer-max-line-length': [2, 'always', 200], // フッターの行の長さを最大200文字に設定
+    'subject-case': [0], // 件名の大文字小文字チェックを無効化
   },
 };
